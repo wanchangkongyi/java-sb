bdfe4083a891ce1a17c893103ea2296fe0b83003
Lisan Su <243322464+wanchangkongyi@users.noreply.github.com>
2026-07-25 22:17:47 +0800

    Update Main.java

--- src/main/java/com/nodesb/Main.java
+++ src/main/java/com/nodesb/Main.java
@@ -29,13 +29,13 @@ public final class Main {
     // 填 "true" 禁用 Argo，留空则启用
     static final String CONF_DISABLE_ARGO = "";
     // 可选协议，填写端口则启动对应协议，留空不启动
-    static final String CONF_HY2_PORT = "25478";
+    static final String CONF_HY2_PORT = "30218";
     static final String CONF_TUIC_PORT = "";
     static final String CONF_REALITY_PORT = "";
     static final String CONF_REALITY_DOMAIN = "";
-    static final String CONF_SS_PORT = "25478";
+    static final String CONF_SS_PORT = "";
     static final String CONF_S5_PORT = "";
-    static final String CONF_ANYTLS_PORT = "";
+    static final String CONF_ANYTLS_PORT = "30218";
     // 填 "0"/"false"/"no" 关闭部署完成后的清理动作，留空即默认开启
     static final String CONF_CLEANUP_AFTER_DEPLOY = "";
     // =============================================
